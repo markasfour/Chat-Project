2943acb87623ef423453df13cddab97f0a67027d
Mark Asfour <mj4sparrow@gmail.com>
2016-03-13 20:31:15 -0700

    created indexes

--- project/sql/src/create_indexes.sql
+++ project/sql/src/create_indexes.sql
@@ -1,3 +1,5 @@
-CREATE INDEX ON chat using HASH (chat_id); 
+CREATE INDEX chat_index ON chat using HASH (chat_id); 
 
-CREATE INDEX ON chat_list using HASH (member);
+CREATE INDEX user_name_index ON usr using HASH (login);
+
+CREATE INDEX list_id_index ON user_list_contains using HASH (list_id);
